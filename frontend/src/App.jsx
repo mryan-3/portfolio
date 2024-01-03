500076320363dd678c880805aa25a09815bc9bc1
mryan-3 <malawaryan@gmail.com>
2024-01-03 14:32:02 +0300

    issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
+import About from './components/About'
+import Contact from './components/Contact'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Project from './components/Projects'
+import Skills from './components/Skills'
 
 function App() {
 
   return (
       <div>
         <Navbar/>
-      <Hero />
+        <Hero />
+       <About />
+       <Skills />
+      <Project />
+      <Contact />
       </div>
  )
 }
