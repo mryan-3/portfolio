098ba137fab1bae8a665f99480a570c44d4d1f75
mryan-3 <malawaryan@gmail.com>
2024-01-03 14:37:35 +0300

    issue

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import { Link } from 'react-scroll';
 import { FaBars, FaTimes, FaLinkedin, FaGithub, } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsPersonFill } from 'react-icons/bs'
-
+import {Link } from 'react-scroll'
 const Navbar = () => {
     const [ nav, setNav ] = useState(false)
     const handleClick = () => setNav(!nav)
