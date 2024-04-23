fd3cf78f90db918fa1c4f463fc128add6217aca9
mryan-3 <malawaryan@gmail.com>
2024-04-23 13:03:26 +0300

    background image add

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -1,9 +1,12 @@
-
 function App() {
-
   return (
-    <>
-    </>
+    <div>
+      <div className='relative h-screen w-full bg-slate-950'>
+        <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]'>
+          <h2 className="text-white text-4xl font-titillium font-bold">Ryan Malawa software engineer </h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
