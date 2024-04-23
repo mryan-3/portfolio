6469f6dce03c66207e594f75ea4898e988811ad8
mryan-3 <malawaryan@gmail.com>
2024-04-23 13:28:03 +0300

    mouse hover on background

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -1,13 +1,63 @@
+import { useRef, useState } from "react"
+
 function App() {
-  return (
-    <div>
-      <div className='relative h-screen w-full bg-slate-950'>
-        <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]'>
-          <h2 className="text-white text-4xl font-titillium font-bold">Ryan Malawa software engineer </h2>
+    const divRef = useRef<HTMLDivElement>(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [opacity, setOpacity] = useState(0);
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (!divRef.current || isFocused) return;
+
+        const div = divRef.current;
+        const rect = div.getBoundingClientRect();
+
+        setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+    };
+
+    const handleFocus = () => {
+        setIsFocused(true);
+        setOpacity(1);
+    };
+
+    const handleBlur = () => {
+        setIsFocused(false);
+        setOpacity(0);
+    };
+
+    const handleMouseEnter = () => {
+        setOpacity(1);
+    };
+
+    const handleMouseLeave = () => {
+        setOpacity(0);
+    };
+
+    return (
+
+        <div className='relative h-screen w-full bg-slate-950'>
+            <div ref={divRef}
+                onMouseMove={handleMouseMove}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                className="to-slate-950 h-full relative overflow-hidden border-slate-800 bg-gradient-to-r from-slate-900 px-8 py-16 shadow-2xl"
+            >
+                <div
+                    className="pointer-events-none absolute -inset-px opacity-0 transition duration-300"
+                    style={{
+                        opacity,
+                        background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(255,255,255,.06), transparent 40%)`,
+                    }}
+                />
+                <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]'>
+                    <h1 className="text-white text-4xl font-titillium font-bold">Bryan Danielson is the greatest to ever do it!!! </h1>
+                    <h1 className="text-white text-4xl font-titillium font-bold">Ryan Malawa is a Software Engineer!!! </h1>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
