b76fa4a84fb054bf0a1a8d951d45e55e6ab8a463
mryan-3 <malawaryan@gmail.com>
2024-09-22 23:02:21 +0300

    scroll

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
     return (
         <>
             <div className='fixed left-0 top-0 -z-10 h-full w-full'>
-                <div className='relative h-full w-full bg-slate-950'>
+                <div className='relative example h-full w-full bg-slate-950'>
                     <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]'></div>
 
                 </div>
